c52df9094eca2d40cb5e356634d7b97f8d83c121
Swechha Prakash <swechhaprakash@gmail.com>
2014-03-26 23:13:53 +0530

    Initial Commit

---
+++ BatApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BatApp
+//
+//  Created by Swechha Prakash on 26/03/14.
+//  Copyright (c) 2014 Swechha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BatApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BatApp
+//
+//  Created by Swechha Prakash on 26/03/14.
+//  Copyright (c) 2014 Swechha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
